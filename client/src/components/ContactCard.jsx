de91267983f055385589b56deec60295342d4ffa
Dhanyllo <danielotchere84@gmail.com>
2025-08-14 17:07:48 +0000

    Added an nginx.conf file

--- client/src/components/ContactCard.jsx
+++ client/src/components/ContactCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaHeart } from "react-icons/fa6";
 import { useNavigation, Link, useLocation } from "react-router-dom";
 
--- client/src/components/FavouriteCard.jsx
+++ client/src/components/FavouriteCard.jsx
@@ -1,41 +1,49 @@
-import React from 'react';
 import { FaHeart } from "react-icons/fa6";
-import { useNavigation , Link , useLocation } from 'react-router-dom';
+import { useNavigation, Link, useLocation } from "react-router-dom";
 
 function FavouriteCard(props) {
   const location = useLocation();
   const Navigation = useNavigation();
-  const isNavigatingToCard = Navigation.location?.pathname === `/detail/${props.id}`;
-  
+  const isNavigatingToCard =
+    Navigation.location?.pathname === `/detail/${props.id}`;
+
   const firstLetter = props.firstName.charAt(0);
   const color = {
-    color:"red"
-  }
+    color: "red",
+  };
 
   return (
-      <div className={props.darkMode ? "card-wrap-darkmode" : "card-wrap-lightmode"}>
-        <div className='btn-layer'>
-          <button className='fav-btn' onClick={() => props.onUpdate(props.id, !props.favouriteStatus)}>
-            <FaHeart style={color} className='heart-icon2' />
-          </button>
-          <Link aria-disabled={isNavigatingToCard} to={`/detail/${props.id}`} state={location.pathname} className="view-link">
-            {isNavigatingToCard ? "...." : "view" }
-          </Link>
-        </div>
-        <div className='card-image-wrap'>
-          <div className='card-image'>
-            <img src="" alt="" />
-            <div className='alpha'>
-              {firstLetter}
-            </div>
-          </div>
-        </div>
-        <div className='card-desc'> 
-          <div>{`${props.firstName} ${props.otherNames}`}</div>
-          <div>{props.phoneNumber}</div>
+    <div
+      className={props.darkMode ? "card-wrap-darkmode" : "card-wrap-lightmode"}
+    >
+      <div className="btn-layer">
+        <button
+          className="fav-btn"
+          onClick={() => props.onUpdate(props.id, !props.favouriteStatus)}
+        >
+          <FaHeart style={color} className="heart-icon2" />
+        </button>
+        <Link
+          aria-disabled={isNavigatingToCard}
+          to={`/detail/${props.id}`}
+          state={location.pathname}
+          className="view-link"
+        >
+          {isNavigatingToCard ? "...." : "view"}
+        </Link>
+      </div>
+      <div className="card-image-wrap">
+        <div className="card-image">
+          <img src="" alt="" />
+          <div className="alpha">{firstLetter}</div>
         </div>
       </div>
-  )
+      <div className="card-desc">
+        <div>{`${props.firstName} ${props.otherNames}`}</div>
+        <div>{props.phoneNumber}</div>
+      </div>
+    </div>
+  );
 }
 
-export default FavouriteCard
+export default FavouriteCard;
