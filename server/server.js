1f0d83143cbff08b9fd4a7a712bbe15bad31c893
Dhanyllo <danielotchere84@gmail.com>
2025-08-22 16:12:48 +0000

    Modified my cors config

--- server/server.js
+++ server/server.js
@@ -5,8 +5,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 const DatabaseConnection = require("./config.js");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Handle preflight
+app.options("*", cors());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 db = DatabaseConnection();
 
